d820f923bf3bc1694dc8e9412f9683bd98a534c8
Koji Furukawa <20028773@tafe.wa.edu.au>
2020-10-18 08:21:43 +0800

    observation class and sql database CRUD added

---
+++ tnats_mobile/tnats_mobile/Models/Observation.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tnats_mobile.Models
+{
+   public class Observation
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public string location { get; set; }
+        public string geo_location { get; set; }
+        public string species { get; set; }
+        public string notes { get; set; }
+        public byte[] photo { get; set; }
+        public bool approved { get; set; }
+        public bool active { get; set; }
+    }
+}
